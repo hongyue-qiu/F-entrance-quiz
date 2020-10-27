4d32faa5b55ca1d822dec3265146b8aab4ef8315
hongyue-qiu <994059641@qq.com>
2020-10-27 23:34:30 +0800

    add students button

--- src/App/App.js
+++ src/App/App.js
@@ -7,26 +7,43 @@ class App extends Component {
         students:[],
         groups:[],
     }
-  render() {
-    return (
-      <div data-testid="app" className="App">
-        <Group />
-          {
-              this.state.groups.map((student, index) => {
-                  return <Group item = {student} key = {`student${index}`}></Group>
-              })
-          }
-          <div className="session">
-            <div className="students">
-                <h2>学员列表</h2>
-                <p>
-                    1.成吉思汗
-                </p>
-            </div>
+    // async componentDidMount() {
+    //     const response = await fetch('http://localhost:8080/students');
+    //     const data = await response.json();
+    //     this.setState({
+    //         students: data,
+    //     })
+    // }
+    handleAddStudent(){
+
+    }
+
+    render() {
+        return (
+          <div data-testid="app" className="App">
+            <Group />
+              {
+                  this.state.groups.map((student) => {
+                      return <Group key = {`student${student.id}`}></Group>
+                  })
+              }
+              <div className="session">
+                <div className="students">
+                    <h2>学员列表</h2>
+                    <p>
+                        1.成吉思汗
+                    </p>
+                    {
+                        this.state.students.map((student) => {
+                            return <p key={`student${student.id}`}>{`${student.id}. ${student.name}`}</p>
+                        })
+                    }
+                    <button className="add-student" onClick={this.handleAddStudent}>+ 添加学员</button>
+                </div>
+              </div>
           </div>
-      </div>
-    );
-  }
+        );
+      }
 }
 
 export default App;
