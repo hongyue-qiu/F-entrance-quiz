36742abadc1aee13008e80dd0357d330d2fc5ae1
hongyue-qiu <994059641@qq.com>
2020-10-27 21:41:32 +0800

    base page css

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Group from './Group';
 
 class App extends Component {
+    state = {
+        students:[],
+        groups:[],
+    }
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Group />
+          {
+              this.state.groups.map((student, index) => {
+                  return <Group item = {student} key = {`student${index}`}></Group>
+              })
+          }
+          <div className="session">
+            <div className="students">
+                <h2>学员列表</h2>
+                <p>
+                    1.成吉思汗
+                </p>
+            </div>
+          </div>
       </div>
     );
   }
