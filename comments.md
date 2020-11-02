59f18345d7c71dd8e48b20291e99e3eb8404209c
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-11-02 14:30:19 +0800

    [mengqiu] add comments

---
+++ comments.md
@@ -0,0 +1,37 @@
+### 完成度：
+
+
+__Details:__
+
+- \- 完成度低于三分之二
+
+### 测试：
+
+
+__Details:__
+
+
+
+### 知识点：
+
+
+__Details:__
+
+- \- state应该放到constructor里面
+- \- button元素不应该放到h2里面
+
+### 工程实践：
+
+
+__Details:__
+
+- \- Eslint error应该fix
+- \- session这个class的名字没有反映业务逻辑
+
+### 综合：
+
+
+__Details:__
+
+- \- 完成度过低，其他维度无法充分考察
+
--- src/App/Group.js
+++ src/App/Group.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class Group extends Component {
+    // TODO GTB-知识点: - state应该放到constructor里面
     state = {
         students:[],
         groups:[],
     }
+    // TODO GTB-工程实践: - Eslint error应该fix
+
     render() {
         return (
+            // TODO GTB-工程实践: - session这个class的名字没有反映业务逻辑
             <div className="session">
+                {/* // TODO GTB-知识点: - button元素不应该放到h2里面 */}
                 <h2>分组列表 <button className="group-stu">分组学员</button></h2>
                 <div className="students students-group">
                     <div className="students-group-title">
